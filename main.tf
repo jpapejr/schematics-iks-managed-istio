5c1bea99730c32e11b86b60bd12312afee94a9d4
John Pape <jpapejr@icloud.com>
2019-08-28 18:21:13 -0400

    corrected formatting for script

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "null_resource" "addons" {
         }
         command = <<EOT
         ibmcloud login -c ${var.account_id} --apikey ${var.ibm_cloud_api_key} -g ${var.resource_group} -r ${var.region} \
-        ibmcloud ks cluster-addon-enable istio --cluster ${var.cluster_name} \
-        ibmcloud ks cluster-addon-enable istio-extras --cluster ${var.cluster_name} \
-        ibmcloud ks cluster-addon-enable istio-sample-bookinfo --cluster ${var.cluster_name} 
+        && ibmcloud ks cluster-addon-enable istio --cluster ${var.cluster_name} \
+        && ibmcloud ks cluster-addon-enable istio-extras --cluster ${var.cluster_name} \
+        && ibmcloud ks cluster-addon-enable istio-sample-bookinfo --cluster ${var.cluster_name} 
         EOT
     }
   
