1cdda96f5f5f5774f1ac1c66543b4dcdda401beb
John Pape <jpapejr@icloud.com>
2019-08-28 18:23:34 -0400

    add line to install ks plugin

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "null_resource" "addons" {
         }
         command = <<EOT
         ibmcloud login -c ${var.account_id} --apikey ${var.ibm_cloud_api_key} -g ${var.resource_group} -r ${var.region} \
+        && ibmcloud plugin install container-service \
         && ibmcloud ks cluster-addon-enable istio --cluster ${var.cluster_name} \
         && ibmcloud ks cluster-addon-enable istio-extras --cluster ${var.cluster_name} \
         && ibmcloud ks cluster-addon-enable istio-sample-bookinfo --cluster ${var.cluster_name} 
